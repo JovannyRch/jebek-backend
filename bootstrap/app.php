0ce5c0fe49a3d075deeaf83ac3f972dc77c46469
JovannyRch <jovannyrch@gmail.com>
2025-03-12 09:09:17 -0600

    update middlewares

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,19 +3,21 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\ForceHttpsMiddleware;
+use App\Http\Middleware\TrustProxies;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
+            /* ForceHttpsMiddleware::class, */
+            TrustProxies::class,
             \App\Http\Middleware\HandleInertiaRequests::class,
-            \App\Http\Middleware\ForceHttpsMiddleware::class,
-            \App\Http\Middleware\TrustProxies::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
